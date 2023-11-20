a37d2c59de3a9d7d6150e1cc31de31842b42674c
ShravaniBathula <121150824+ShravaniBathula@users.noreply.github.com>
2023-11-20 19:32:59 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class sample {
     public static void main(String[] args) {
-        System.out.println("Hello world , it's sample docker image");
+        System.out.println("Hello world");
     }
 }
